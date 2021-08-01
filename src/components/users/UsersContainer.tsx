2c6fb016b4a913a092b9731f29b8c1861faa09d8
faceless49 <digital.zeus@mail.ru>
2021-08-01 21:06:54 +0300

    del Api Component users. Make clear func Users

--- src/components/users/UsersContainer.tsx
+++ src/components/users/UsersContainer.tsx
@@ -1,7 +1,7 @@
-import { connect } from "react-redux";
-import UsersC from "./UsersC";
-import { AppStateType } from "../../redux/redux-store";
-import { Dispatch } from "redux";
+import {connect} from 'react-redux';
+import UsersAPIComponent from './UsersAPIComponent';
+import {AppStateType} from '../../redux/redux-store';
+import {Dispatch} from 'redux';
 import {
   followAC,
   setUsersAC,
@@ -9,6 +9,10 @@ import {
   UserType,
   setCurrentPageAC, setTotalUsersCountAC,
 } from '../../redux/users-reducer';
+import React from 'react';
+import axios from 'axios';
+import {Users} from './Users';
+
 
 type MapStatePropsType = {
   users: Array<UserType>;
@@ -25,6 +29,46 @@ type MapDispatchToProps = {
 };
 
 export type UsersPropsType = MapStatePropsType & MapDispatchToProps;
+
+class UsersAPIComponent extends React.Component<UsersPropsType> {
+  // constructor(props:GetUsersResponseType) {
+  //   super(props);
+  // }
+  componentDidMount() {
+    // Единственное правильное место где надо делать сайд эффект
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+        this.props.setTotalUsersCount(response.data.totalCount);
+      });
+  }
+
+  onPageChanged = (pageNumber: number) => {
+    this.props.setCurrentPage(pageNumber)
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+      });
+  }
+
+  render() {
+
+
+    return <Users totalUsersCount={this.props.totalUsersCount}
+                  pageSize={this.props.pageSize}
+                  currentPage={this.props.currentPage}
+                  onPageChanged={this.onPageChanged}
+                  users={this.props.users}
+                  follow={this.props.follow}
+                  unfollow={this.props.unfollow}
+
+    />
+  }
+}
 
 let mapStateToProps = (state: AppStateType): MapStatePropsType => {
   return {
@@ -58,5 +102,5 @@ let mapDispatchToProps = (dispatch: Dispatch): MapDispatchToProps => {
 // @ts-ignore
 export const UsersContainer = connect(
   mapStateToProps,
-  mapDispatchToProps
-)(UsersC);
+  mapDispatchToProps)
+(UsersAPIComponent);
