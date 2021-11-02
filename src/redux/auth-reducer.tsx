e38fdb80904a7d196e2e6cd88d3ee33832afdd89
faceless49 <digital.zeus@mail.ru>
2021-11-02 19:30:03 +0300

    refactor promises auth reducer in async await

--- src/redux/auth-reducer.tsx
+++ src/redux/auth-reducer.tsx
@@ -3,7 +3,7 @@ import { authApi } from "../api/api";
 import { stopSubmit } from "redux-form";
 import { Dispatch } from "redux";
 
-const SET_USER_DATA = "SET_USER_DATA";
+const SET_USER_DATA = "auth/SET_USER_DATA";
 
 export type AuthInitialStateType = {
   userId: number | null;
@@ -12,7 +12,6 @@ export type AuthInitialStateType = {
   isAuth: boolean;
 };
 
-// export type AuthInitialStateType = typeof authInitialState;
 let authInitialState = {
   userId: null as number | null,
   email: null as string | null,
@@ -46,36 +45,47 @@ export const setAuthUserData = (
     payload: { userId, email, login, isAuth },
   } as const);
 
-export const getAuthUserData = () => (dispatch: Dispatch<ActionsTypes>) => {
-  authApi.me().then((response) => {
+// * Promise with then
+// export const getAuthUserData = () => (dispatch: Dispatch<ActionsTypes>) => {
+//   authApi.me().then((response) => {
+//     if (response.data.resultCode === 0) {
+//       let { id, email, login } = response.data.data;
+//       dispatch(setAuthUserData(id, email, login, true));
+//     }
+//   });
+// };
+
+// * Async await
+export const getAuthUserData =
+  () => async (dispatch: Dispatch<ActionsTypes>) => {
+    let response = await authApi.me();
+
     if (response.data.resultCode === 0) {
       let { id, email, login } = response.data.data;
       dispatch(setAuthUserData(id, email, login, true));
     }
-  });
-};
-
-export const login =
-  (email: string, password: string, rememberMe: boolean) => (dispatch: any) => {
-    authApi.login(email, password, rememberMe).then((response) => {
-      if (response.data.resultCode === 0) {
-        dispatch(getAuthUserData());
-      } else {
-        let message =
-          response.data.messages.length > 0
-            ? response.data.messages[0]
-            : "Some error";
-        dispatch(stopSubmit("login", { _error: message }));
-      }
-    });
   };
 
-export const logout = () => (dispatch: Dispatch<ActionsTypes>) => {
-  authApi.logout().then((response) => {
+export const login =
+  (email: string, password: string, rememberMe: boolean) =>
+  async (dispatch: any) => {
+    let response = await authApi.login(email, password, rememberMe);
     if (response.data.resultCode === 0) {
-      dispatch(setAuthUserData(null, null, null, false));
+      dispatch(getAuthUserData());
+    } else {
+      let message =
+        response.data.messages.length > 0
+          ? response.data.messages[0]
+          : "Some error";
+      dispatch(stopSubmit("login", { _error: message }));
     }
-  });
+  };
+
+export const logout = () => async (dispatch: Dispatch<ActionsTypes>) => {
+  let response = await authApi.logout();
+  if (response.data.resultCode === 0) {
+    dispatch(setAuthUserData(null, null, null, false));
+  }
 };
 
 export default authReducer;
