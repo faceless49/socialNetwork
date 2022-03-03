32b6c74af464ec738d8dfd28a1af426598f3fbf8
faceless49 <digital.zeus@mail.ru>
2022-03-03 20:13:49 +0300

    Finish 11L 2.0

--- src/components/Dialogs/DialogItem/DialogItem.tsx
+++ src/components/Dialogs/DialogItem/DialogItem.tsx
@@ -1,13 +1,14 @@
 import s from "./../Dialogs.module.scss";
 import { NavLink } from "react-router-dom";
 import { DialogType } from "../../../types/types";
+import React from "react";
 
-const DialogItem = (props: DialogType) => {
-  let path = "/dialogs/" + props.id;
+const DialogItem: React.FC<DialogType> = ({ id, name }) => {
+  let path = "/dialogs/" + id;
 
   return (
     <div className={s.dialog + " " + s.active}>
-      <NavLink to={path}>{props.name}</NavLink>
+      <NavLink to={path}>{name}</NavLink>
     </div>
   );
 };
